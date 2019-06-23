0f6717f2ffb9db7401de904764e42693481c9683
Yoshi 66 <yoshi66.tm@gmail.com>
2019-06-24 00:14:13 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Media_Downloader
+{
+    public partial class MainWindow : Window
+    {
+        private string YoutubedlPath;
+
+        public MainWindow()
+        {
+            YoutubedlPath = AppDomain.CurrentDomain.BaseDirectory + @"Youtube-dl\";
+            InitializeComponent();
+        }
+
+        private void Btn_Descargar(object sender, RoutedEventArgs e)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = YoutubedlPath + "Youtube-dl.exe";
+            p.StartInfo.Arguments = "-U";
+            p.Start();
+        }
+    }
+}
